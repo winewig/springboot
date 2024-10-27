51b03db58f3511103384a9c91e6007f6b2c0266b
qinyuanZhang <qinyuan.zhang@elektrobit.com>
2024-10-27 16:01:48 +0100

    First spring boot project

---
+++ src/main/java/com/example/demo/SayHelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello! What are you learning today?";
+    }
+
+    @RequestMapping("say-hello-jsp")
+    public String sayHelloJsp() {
+        return "sayHello";
+    }
+}
